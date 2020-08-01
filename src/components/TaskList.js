beba29ab59a3cc27a99e93a04ef0e8686fa2e5c9
Abbah Anoh <anohabbah@gmail.com>
2020-08-01 21:24:34 +0200

    Build out states

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -13,13 +13,40 @@ import Task from "./Task";
 function TaskList({ loading, tasks, onPinTask, onArchivedTask }) {
     const events = { onPinTask, onArchivedTask };
 
-    if (loading) return <div className="list-items">loading</div>
+    const LoadingRow = (
+        <div className="loading-item">
+            <span className="glow-checkbox"/>
+            <span className="glow-text">
+                <span>Loading</span> <span>cool</span> <span>state</span>
+            </span>
+        </div>
+    )
 
-    if (tasks.length === 0) return <div className="list-items">empty</div>
+    if (loading) return <div className="list-items">
+        <LoadingRow/>
+        <LoadingRow/>
+        <LoadingRow/>
+        <LoadingRow/>
+        <LoadingRow/>
+        <LoadingRow/>
+    </div>
 
-    return (
+    if (tasks.length === 0) return <div className="list-items">
+        <div className="wrapper-message">
+            <span className="icon-check" />
+            <div className="title-message">You have no tasks</div>
+            <div className="subtitle-message">Sit back and relax</div>
+        </div>
+    </div>
+
+    const tasksInOrder = [
+        ...tasks.filter(t => t.state === 'TASK_PINNED'),
+        ...tasks.filter(t => t.state !== 'TASK_PINNED'),
+    ];
+
+    return (            
         <div className="list-items">
-            {tasks.map(task => <Task key={task.id} task={task} {...events} />)}
+            {tasksInOrder.map(task => <Task key={task.id} task={task} {...events} />)}
         </div>
     )
 }
